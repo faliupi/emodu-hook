653dab14216028c69ac24dd1611b73ec95f50e87
falih <101558443+faliupi@users.noreply.github.com>
2025-06-22 19:45:40 +0700

    Add files via upload

---
+++ dist/src/constants.ts.js
@@ -0,0 +1,22 @@
+const AUTH0_APP_DOMAIN = "dev-0g1q2v5xj3z4.us.auth0.com";
+const AUTH0_CLIENT_ID = "2g1q2v5xj3z4";
+const AUTH0_AUDIENCE = "https://emodu-api.com";
+const BE_ENDPOINT = "https://rwu.my.id";
+const BE_ENDPOINT_V2 = "http://localhost:8080/api/v2";
+const EMOVALARO_MODDEL_ENDPOINT = "https://emovalaro7-service-7qkc4rj5aa-et.a.run.app/predict";
+const OPENAAI_API_URL = "https://affective-intervention-openai-189935831356.us-central1.run.app/openai";
+const LLAMAAI_API_URL = "https://ai-llama-app-189935831356.asia-southeast1.run.app/ai-llama";
+const EMODU_WEB_URL = "https://emodu-v2.vercel.app";
+const DROWSINESS_DETECT_API_URL = "http://localhost:5000/detect-drowsiness";
+export {
+  AUTH0_APP_DOMAIN,
+  AUTH0_CLIENT_ID,
+  AUTH0_AUDIENCE,
+  BE_ENDPOINT,
+  BE_ENDPOINT_V2,
+  EMOVALARO_MODDEL_ENDPOINT,
+  OPENAAI_API_URL,
+  LLAMAAI_API_URL,
+  EMODU_WEB_URL,
+  DROWSINESS_DETECT_API_URL
+};
